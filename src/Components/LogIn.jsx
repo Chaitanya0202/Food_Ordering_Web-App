a198d44c091f12c5374dc6e0f1eb6fefe8e3fae4
Chaitanya0202 <115619207+Chaitanya0202@users.noreply.github.com>
2024-01-26 15:55:35 +0530

    modified log in and sign up page

--- src/Components/LogIn.jsx
+++ src/Components/LogIn.jsx
@@ -1,115 +1,100 @@
-
 import axios from "axios";
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
 import { jwtDecode } from "jwt-decode";
 import { useGlobelContext } from "../Context/ProductContext";
+import GoogleLoginComponent from "./GoogleLoginComponent";
 
 function LogIn() {
-    let navigate=useNavigate()
+  let navigate = useNavigate();
   const [logInfo, setLogInfo] = useState({
     email: "",
     password: "",
   });
   const { email, password } = logInfo;
-  const { count, cart,loginData,setLoginData } = useGlobelContext();
+  const { count, cart, loginData, setLoginData } = useGlobelContext();
 
   const onInputChange = (e) => {
     setLogInfo({ ...logInfo, [e.target.name]: e.target.value });
     // console.log(email);
     // console.log(password);
   };
-  const isLogedIn=()=>{
-    console.log("lOG IN sUCCESS")
-        
+  const isLogedIn = () => {
+    console.log("lOG IN sUCCESS");
+
     const isLogedIn = localStorage.getItem("isLogedIn");
     localStorage.setItem("isLogedIn", true);
-    navigate("/cartPage")
+    navigate("/cartPage");
 
-    console.log(isLogedIn)
-  }
-  const onSubmitHandler= async(e)=>{
+    console.log(isLogedIn);
+  };
+  const onSubmitHandler = async (e) => {
     e.preventDefault();
-    
+
     try {
-      const result = await axios.get(`http://localhost:8080/getUserByEmail/${email}`);
-    
-    
-    if(result.data.password===password){
-        
-      isLogedIn()
-       
-    }
-    else{
-      alert("Invalid Email")
-    }
+      const result = await axios.get(
+        `http://localhost:8080/getUserByEmail/${email}`
+      );
+
+      if (result.data.password === password) {
+        isLogedIn();
+      } else {
+        alert("Invalid Email");
+      }
     } catch (error) {
-      console.log(error)
-      alert("Enter Valid Creadintial..!")
+      console.log(error);
+      alert("Enter Valid Creadintial..!");
     }
-  }
+  };
 
   return (
     <>
-    
-    <div className="container mt-5">
-    
-    <h3 className="tex-center text-bold ">lOG iN Page</h3>
-      <form  onSubmit={(e)=>onSubmitHandler(e)}>
-        <div className="row">
-          <div className="col-md-6 offset-md-3 border rounded p-4 mt2 shadow">
-          
-            <div className="mb-3">
-            
-              <label htmlFor="email" className="form-label">
-                Email
-              </label>
-              <input
-                type="email"
-                className="form-control"
-                placeholder="Enter Your Email"
-                name="email"
-                value={email}
-                onChange={(e) => onInputChange(e)}
-              ></input>
+      <div className="container mt-5">
+        <h3 className="tex-center text-bold ">lOG iN </h3>
+        <form onSubmit={(e) => onSubmitHandler(e)}>
+          <div className="row">
+            <div className="col-md-6 offset-md-3 border rounded p-4 mt2 shadow">
+              <div className="form-floating mt-3">
+                <input
+                  type="email"
+                  className="form-control"
+                  placeholder="Enter Your Email"
+                  name="email"
+                  value={email}
+                  onChange={(e) => onInputChange(e)}
+                ></input>
+                <label htmlFor="email" className="form-label">
+                  Email
+                </label>
+              </div>
+              <div className="form-floating mt-3 ">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Enter Your Password"
+                  name="password"
+                  value={password}
+                  onChange={(e) => onInputChange(e)}
+                ></input>
+                <label htmlFor="pass" className="form-label mx-4">
+                  Password
+                </label>
+              </div>
+              <div className="d-flex justify-content-center mt-3">
+              
+              <GoogleLoginComponent />
+              </div>
+
+              <button type="submit" className="btn btn-primary mt-4">
+                Log In
+              </button>
+              <Link className="btn btn-outline-danger mx-2 mt-4" to={"/"}>
+                Cancel
+              </Link>
             </div>
-            <div className="mb-3">
-              <label htmlFor="pass" className="form-label mx-4">
-                Password
-              </label>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Enter Your Password"
-                name="password"
-                value={password}
-                onChange={(e) => onInputChange(e)}
-              ></input>
-            </div>
-           
-            <button type="submit" className="btn btn-primary mb-4">
-              Log In
-            </button>
-            <Link className="btn btn-outline-danger mx-2 mb-4"   to={"/"}>Cancel</Link>
-            <GoogleOAuthProvider clientId="413016402359-b165dqc2v0lp0gcmses7paj9mrrvgvm7.apps.googleusercontent.com">
-            <GoogleLogin
-              onSuccess={(credentialResponse) => {
-                const decoded = jwtDecode(credentialResponse.credential);
-                setLoginData(decoded)
-                console.log(decoded);
-                isLogedIn()
-                console.log("log in data is ",loginData)
-              }}
-              onError={() => {
-                console.log("Login Failed");
-              }}
-            />
-          </GoogleOAuthProvider>
           </div>
-        </div>
         </form>
-        
       </div>
     </>
   );
--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -1,10 +1,7 @@
-
 import axios from "axios";
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
-import { jwtDecode } from "jwt-decode";
-import GoogleLoginComponent from "./GoogleLogin";
+import GoogleLoginComponent from "./GoogleLoginComponent";
 
 export default function SignUp() {
   let navigate = useNavigate();
@@ -20,86 +17,99 @@ export default function SignUp() {
 
   const onInputChange = (e) => {
     setUser({ ...user, [e.target.name]: e.target.value });
-    
   };
 
   const onSubmitHandler = async (e) => {
     e.preventDefault();
-    console.log(user)
-    await axios.post(`http://localhost:8080/saveUser`,user);
+    console.log(user);
+    await axios.post(`http://localhost:8080/saveUser`, user);
     navigate("/logIn");
   };
 
-
-  
   return (
     <div className="container mt-5">
       <div className="row">
         <div className="col-md-6 offset-md-3 border rounded p-4 mt2 shadow">
-          <h3 className="tex-center text-bold ">Sign Up Page</h3>
+          <h3 className="tex-center text-bold ">Sign Up </h3>
           <form
             onSubmit={(e) => onSubmitHandler(e)}
             className="g-3  needs-validation"
             novalidate
           >
             <div className="mb-3">
-              <div>
-                <label htmlFor="Name" className="form-label">
-                  Name
-                </label>
+              <div className="form-floating mt-3">
                 <input
                   type="text"
                   className="form-control"
                   placeholder="Enter Your Name"
                   name="name"
                   value={name}
-                  onChange={(e) => onInputChange(e)} required minLength={"2"}
+                  onChange={(e) => onInputChange(e)}
+                  required
+                  minLength={"2"}
                 ></input>
-                <div class="valid-feedback">Looks good!</div>
+                <label htmlFor="Name" className="form-label">
+                  Name
+                </label>
+                <div className="valid-feedback">Looks good!</div>
               </div>
-              <label htmlFor="email" className="form-label">
-                Email
-              </label>
-              <input
-                type="email"
-                className="form-control"
-                placeholder="Enter Your Email"
-                name="email"
-                value={email}
-                onChange={(e) => onInputChange(e)} required
-              ></input>
-
-              <label htmlFor="pass" className="form-label">
-                Password
-              </label>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Enter Your Password"
-                name="password"
-                value={password}
-                onChange={(e) => onInputChange(e)} required minLength={"4"} maxLength={"8"}
-              ></input>
-              <label htmlFor="phone" className="form-label">
-                Phone
-              </label>
+              <div className="form-floating mt-3">
               <input
                 type="text"
                 className="form-control"
                 placeholder="Enter Your Phone"
                 name="phone"
                 value={phone}
-                onChange={(e) => onInputChange(e)} required
-                maxlength="10" minLength={"6"}
+                onChange={(e) => onInputChange(e)}
+                required
+                maxlength="10"
+                minLength={"6"}
               ></input>
+              <label htmlFor="phone" className="form-label">
+                Phone
+              </label>
             </div>
+              <div className="form-floating mt-3">
+                <input
+                  type="email"
+                  className="form-control"
+                  placeholder="Enter Your Email"
+                  name="email"
+                  value={email}
+                  onChange={(e) => onInputChange(e)}
+                  required
+                ></input>
+                <label htmlFor="email" className="form-label">
+                  Email
+                </label>
+              </div>
+              <div className="form-floating mt-3">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Enter Your Password"
+                  name="password"
+                  value={password}
+                  onChange={(e) => onInputChange(e)}
+                  required
+                  minLength={"4"}
+                  maxLength={"8"}
+                ></input>
+                <label htmlFor="pass" className="form-label">
+                  Password
+                </label>
+              </div>
+            </div>
+            <div className="d-flex justify-content-center m-3">
+            <GoogleLoginComponent />
+            </div>
+
             <button type="submit" className="btn btn-outline-primary m-2">
               Submit
             </button>
             <Link className="btn btn-outline-danger m-2" to={"/"}>
               Cancel
             </Link>
-            <GoogleLoginComponent/>
           </form>
         </div>
       </div>
