2519fcba5fce41fdbf6abd0650fb035b05b44471
Chaitanya0202 <115619207+Chaitanya0202@users.noreply.github.com>
2024-08-02 11:35:20 +0530

    added

--- src/Components/AddUser.jsx
+++ src/Components/AddUser.jsx
@@ -17,8 +17,7 @@ export default function AddUser() {
   const onInputChange = (e) => {
     setUser({ ...user, [e.target.name]: e.target.value });
 
-
-    // const result = await axios.get(`http://localhost:8080/getUserById/${id}`);
+    
     // const result = await axios.get(`http://localhost:8080/getUserById/${id}`);
   };
 
